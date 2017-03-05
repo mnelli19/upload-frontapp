2a80a2b4771a00c7b85fe8516e07f01995fbab77
Mario Nelli <mario@MacBook-Air-di-Mario.local>
2017-03-05 15:12:42 +0100

    new

--- target/upload-frontapp-1.0-SNAPSHOT/js/app.js
+++ target/upload-frontapp-1.0-SNAPSHOT/js/app.js
@@ -46,7 +46,7 @@
 	
     var r = new Flow({
         target: "https://upload-flowjs-java.mybluemix.net/upload",
-    	method: "octet",
+    	method: "multipart",
 //    	target:'https://upload-flowjs-node.mybluemix.net/upload',
         chunkSize: 1024 * 1024,
         testChunks: false,
